d5ed6595ea4aa462946cbbaa23198c25f5e5c039
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-11-04 09:15:55 +0600

    footer and header added done

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,13 @@
 import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
 export {
     Hero,
+    CustomButton,
+    Navbar,
+    Footer
+
     
 }
